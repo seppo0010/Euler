c383b0132ab830d7b29bfcaa1ae8f35290a18c68
Sebastian Waisbrot <sebastian@brooklynpacket.com>
2011-01-09 01:14:51 -0300

    Solved problem 55

--- 55/main.c
+++ 55/main.c
@@ -4,7 +4,6 @@
 #include <bigint.h>
 #include <string.h>
 
-
 char * bi_to_str(bigint num) {
 	char* tmp;
 	int pos = 0;
@@ -15,14 +14,40 @@ char * bi_to_str(bigint num) {
 	} else {
 		tmp = malloc(sizeof(char) * 2);
 	}
-	tmp[pos] = bi_int_mod( num, 10 ) + '0';
+	tmp[pos] = bi_int_mod( bi_copy(num), 10 ) + '0';
 	tmp[pos+1] = '\0';
+	bi_free(num);
 	return tmp;
 }
 
 bigint bi_revert(bigint num) {
+	char *str = bi_to_str(bi_copy(num));
+	int digits = strlen(str);
+	if (digits < 2) { free(str); return num; }
+	char *ret = malloc(sizeof(char) * digits + 1);
+	int i;
+	for (i=0;i<digits;++i) {
+		ret[digits-i-1] = str[i];
+	}
+	ret[digits] = '\0';
+	bigint bi = str_to_bi(ret);
+	free(str);
+	free(ret);
+	bi_free(num);
+	return bi;
 }
+
 int bi_is_palindromic(bigint num) {
+	char *str = bi_to_str(bi_copy(num));
+	int digits = strlen(str);
+	int i;
+	int ret = 1;
+	for (i=0;i<digits/2;++i) {
+		if (str[i] != str[digits-i-1]) { ret = 0; break; }
+	}
+	free(str);
+	bi_free(num);
+	return ret;
 }
 
 int is_palindromic(int num) {
@@ -55,34 +80,29 @@ int main() {
 	int n;
 	bi_initialize();
 	bigint n2;
-	for (i=9999;i>=1;--i) {
-		if (lychrels[i] == 0) continue;
-		else if (lychrels[i] == 1) {
-			++c;
-			continue;
-		} else {
+	for (i=10000;i>=10;--i) {
+		if (lychrels[i] == -1) {
 			n = i;
 			for (j=0;j<50;++j) {
 				if (n > 0) {
 					n += revert(n);
 					if (n > 100000000) {
 						n2 = int_to_bi(n);
+						n = 0;
 						--j;
 						continue;
 					}
-					if (n < 10000 && lychrels[n] >= 0) {
-						c += lychrels[n];
+					if (n < 10000 && lychrels[n] == 0) {
+						lychrels[i] = 0;
 						break;
 					} else if (is_palindromic(n)) {
-						++c;
-						lychrels[i] = 1;
+						lychrels[i] = 0;
 						break;
 					}
 				} else {
-					n2 = bi_add(n2, bi_revert(n2));
-					if (bi_is_palindromic(n2)) {
-						++c;
-						lychrels[i] = 1;
+					n2 = bi_add(n2, bi_revert(bi_copy(n2)));
+					if (bi_is_palindromic(bi_copy(n2))) {
+						lychrels[i] = 0;
 						bi_free(n2);
 						break;
 					} else if (j == 49) {
@@ -91,8 +111,12 @@ int main() {
 					}
 				}
 			}
-			if (lychrels[i] == -1) lychrels[i] = 0;
+			if (lychrels[i] == -1) lychrels[i] = 1;
 		}
+		c += lychrels[i];
+#ifdef DEBUG
+		printf("%d? %d\n", i, lychrels[i]);
+#endif
 	}
 	printf("%d\n", c);
 	bi_terminate();
