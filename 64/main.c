bdd2616007a244998df976f9c5b21efc0dbcc38c
Sebastian Waisbrot <sebastian@brooklynpacket.com>
2011-02-22 14:53:33 -0300

    Passed problem 64

--- 64/main.c
+++ 64/main.c
@@ -9,37 +9,32 @@ int main() {
 	//11, 13
 	int i=23, a=1,b,c,d,e,f=1;
 	int den, starte, startf;
-	//for (i=2;i<14;i++)
+	int count = 0;
+	int hhhh;
+	for (i=2;i<10001;i++)
 	{
 		e = a = sqrt(i);
-		int min = a;
-		den = c = i - min;
+		den = c = i - a;
 		f = 1;
-		//if (sqrt(i) * sqrt(i) == i) continue;
-		int hhhh = 0;
+		if ((int)sqrt(i) * (int)sqrt(i) == i) continue;
+		hhhh = 0;
 		starte = e;
 		startf = f;
 		do {
 			a = f;
 			b = e;
 			c = i - e*e;
-			//d = (int)ceil((double)a*b / den);
+			d = floor((double)f / (sqrt(i) - b));
 			f = c / a;
-			d = (int)ceil((double)b/(c/a));
 			e = f * d - b;
-			printf("e %d\n", e);
-			while (e < min-1) {
-				d+=1;
-				e = f * d - b;
-				printf("%d %d %d %d\n", f,d,b,e);
-				printf("%d\n", min);
-			}
-			printf("%d/%d/%d = %d\n", b,c,a,d);
-			//if (c / a * d - b == 0) d = (int)ceil((double)a*b*2 / den);
-			printf("%d*(sqrt(i)+%d)/%d=%d+(sqrt(i)-%d)/%d\n", a,b,c,d,e,f);
+			//printf("e %d\n", e);
+			//printf("%d/%d/%d = %d\n", b,c,a,d);
+			//printf("%d*(sqrt(i)+%d)/%d=%d+(sqrt(i)-%d)/%d\n", a,b,c,d,e,f);
 			if (e == starte && f == startf) break;
 		} while (++hhhh < 1000);
-		printf("%d\n", 1+hhhh);
+		//printf("%d %d\n", i, 1+hhhh);
+		if (1+hhhh % 2 == 1) ++count;
 	}
+	printf("%d\n", count);
 	return 0;
 }
