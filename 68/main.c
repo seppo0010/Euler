ae3c5793275726c2ec054cc406016b5e8e792a61
Sebastian Waisbrot <seppo0010@gmail.com>
2011-02-25 06:12:16 -0300

    Finish 68, but the answer is wrong, some minor detail probably missing

--- 68/main.c
+++ 68/main.c
@@ -10,7 +10,79 @@ int main() {
 		n[i] = 9-i;
 
 	char str[18];
+	int v[4];
+	int found = 0;
 	while (1) {
+		for (n[0]=9;n[0]>=0;n[0]--) {
+			for (n[1]=9;n[1]>=0;n[1]--) {
+				if (n[0] == n[1]) continue;
+				for (n[2]=9;n[2]>=0;n[2]--) {
+					if (n[0] == n[2]) continue;
+					if (n[1] == n[2]) continue;
+					v[0] = n[0] + n[1] + n[2];
+					if (n[0] == 0 || n[1] == 0 || n[2] == 0) v[0] += 10;
+					for (n[3]=9;n[3]>=0;n[3]--) {
+						if (n[0] == n[3]) continue;
+						if (n[1] == n[3]) continue;
+						if (n[2] == n[3]) continue;
+						for (n[4]=9;n[4]>=0;n[4]--) {
+							if (n[0] == n[4]) continue;
+							if (n[1] == n[4]) continue;
+							if (n[2] == n[4]) continue;
+							if (n[3] == n[4]) continue;
+							v[1] = n[3] + n[2] + n[4];
+							if (n[3] == 0 || n[2] == 0 || n[4] == 0) v[1] += 10;
+							if (v[0] != v[1]) continue;
+							for (n[5]=9;n[5]>=0;n[5]--) {
+								if (n[0] == n[5]) continue;
+								if (n[1] == n[5]) continue;
+								if (n[2] == n[5]) continue;
+								if (n[3] == n[5]) continue;
+								if (n[4] == n[5]) continue;
+								for (n[6]=9;n[6]>=0;n[6]--) {
+									if (n[0] == n[6]) continue;
+									if (n[1] == n[6]) continue;
+									if (n[2] == n[6]) continue;
+									if (n[3] == n[6]) continue;
+									if (n[4] == n[6]) continue;
+									if (n[5] == n[6]) continue;
+									v[2] = n[5] + n[4] + n[6];
+									if (n[5] == 0 || n[4] == 0 || n[6] == 0) v[2] += 10;
+									if (v[0] != v[2]) continue;
+									for (n[7]=9;n[7]>=0;n[7]--) {
+										if (n[0] == n[7]) continue;
+										if (n[1] == n[7]) continue;
+										if (n[2] == n[7]) continue;
+										if (n[3] == n[7]) continue;
+										if (n[4] == n[7]) continue;
+										if (n[5] == n[7]) continue;
+										if (n[6] == n[7]) continue;
+										for (n[8]=9;n[8]>=0;n[8]--) {
+											if (n[0] == n[8]) continue;
+											if (n[1] == n[8]) continue;
+											if (n[2] == n[8]) continue;
+											if (n[3] == n[8]) continue;
+											if (n[4] == n[8]) continue;
+											if (n[5] == n[8]) continue;
+											if (n[6] == n[8]) continue;
+											if (n[7] == n[8]) continue;
+											v[3] = n[7] + n[6] + n[8];
+											if (n[7] == 0 || n[6] == 0 || n[8] == 0) v[3] += 10;
+											if (v[0] != v[3]) continue;
+											for (n[9]=9;n[9]>=0;n[9]--) {
+												if (n[0] == n[9]) continue;
+												if (n[1] == n[9]) continue;
+												if (n[2] == n[9]) continue;
+												if (n[3] == n[9]) continue;
+												if (n[4] == n[9]) continue;
+												if (n[5] == n[9]) continue;
+												if (n[6] == n[9]) continue;
+												if (n[7] == n[9]) continue;
+												if (n[8] == n[9]) continue;
+												v[4] = n[9] + n[8] + n[1];
+												if (n[9] == 0 || n[8] == 0 || n[1] == 0) v[4] += 10;
+												if (v[0] != v[4]) continue;
+
 		if (n[2] == 0 || n[4] == 0 || n[6] == 0) continue;
 		for (i=0;i<18;i++) {
 			str[i] = 0;
@@ -39,7 +111,28 @@ int main() {
 			}
 			++pos;
 		}
+		found = 1;
 		break;
+											}
+											if (found) break;
+										}
+										if (found) break;
+									}
+									if (found) break;
+								}
+								if (found) break;
+							}
+							if (found) break;
+						}
+						if (found) break;
+					}
+					if (found) break;
+				}
+				if (found) break;
+			}
+			if (found) break;
+		}
+		if (found) break;
 	}
 
 	printf("%s\n", str);
